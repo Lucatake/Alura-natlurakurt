ad49af84048d0a0752151bf42a7d626bd56cc21c
Luana <1295943@sga.pucmias.br>
2021-07-12 19:28:27 -0300

    Aula 1: Components com React, Styled Components e NextJS

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,66 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
-//const Title = styled.h1`
-//  font-size: 50px;
-//  color: ${({ theme }) => theme.colors.primary};
-//`
-
-const Box = styled.div`
-  background: #FFFFFF;
-  border-radius: 8px;
-  padding: 16px;
-  /* CSS Pré-Pronto */
-  margin-bottom: 10px;
-  .boxLink {
-    font-size: 14px;
-    color: #2E7BB4;
-    text-decoration: none;
-    font-weight: 800;
-  }
-  .title {
-    font-size: 32px;
-    font-weight: 400;
-    margin-bottom: 20px;
-  }
-  .subTitle {
-    font-size: 18px;
-    font-weight: 400;
-    margin-bottom: 20px;
-  }
-  .smallTitle {
-    margin-bottom: 20px;
-    font-size: 16px;
-    font-weight: 700;
-    color: #333333;
-    margin-bottom: 20px;
-  }
-  hr {
-    margin-top: 12px;
-    margin-bottom: 8px;
-    border-color: transparent;
-    border-bottom-color: #ECF2FA;
-  }
-  input {
-    width: 100%;
-    background-color: #F4F4F4;
-    color: #333333;
-    border: 0;
-    padding: 14px 16px;
-    margin-bottom: 14px;
-    border-radius: 10000px;
-    ::placeholder {
-      color: #333333;
-      opacity: 1;
-    }
-  }
-  button {
-    border: 0;
-    padding: 8px 12px;
-    color: #FFFFFF;
-    border-radius: 10000px;
-    background-color: #6F92BB;
-  }
-`;
-
-const MainGrid = styled.main`
-  width: 100%;
-  grid-gap: 10px;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 500px;
-  padding: 16px;
-  .profileArea {
-    display: none;
-    @media(min-width: 860px) {
-      display: block;
-    }
-  }
-  @media(min-width: 860px) {
-    max-width: 1110px;
-    display: grid;
-    grid-template-areas: 
-      "profileArea welcomeArea profileRelationsArea";
-    grid-template-columns: 160px 1fr 312px;
-  }
-`;
-
+function ProfileSidebar(propriedades) {
+  console.log(propriedades);
+  return (
+    <Box>
+      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
 
 export default function Home() {
+  const usuarioAleatorio = 'Lucatake';
+  const pessoasFavoritas = [
+    'juunegreiros',
+    'ludigoncalves',
+    'PyLadiesBH',
+    'mllbueno',
+    'izabelacborges',
+    'ycwhencpp'
+  ]
+
   return (
-  <MainGrid>
-    <div style={{gridArea: 'profileArea'}}>
-      <Box>
-        <img src="https://github.com/Lucatake.png"/>
-      </Box>
-    </div>
-    
-    <div style={{gridArea: 'welcomeArea'}}>
-      <Box>
-        Bem Vinda!
-      </Box>
-      <Box>
-        O que você deseja fazer?
-      </Box>
-    </div>
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        {/* <Box style="grid-area: profileArea;"> */}
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={usuarioAleatorio} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">
+              Bem vindo(a) 
+            </h1>
 
-    <div style={{gridArea: 'profileRelationsArea'}}>
-      <Box>
-        Meus amigos (6)
-      </Box>
-      <Box>
-        Minhas comunidades
-      </Box>
-    </div>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({pessoasFavoritas.length})
+            </h2>
 
-  </MainGrid>
-  
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
   )
 }
